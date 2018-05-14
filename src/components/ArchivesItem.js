742ec2aa3a5fec0f7ecb2d395f2905765a8951fc
WeiKee <weikee070194@gmail.com>
2018-05-14 23:34:19 +0800

    init post structure

--- src/components/ArchivesItem.js
+++ src/components/ArchivesItem.js
@@ -1,16 +1,69 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import InitProject from '../components/post/initProject';
+
 const ArchivesItem = (props) => {
-    console.log(props);
+    console.log(props.match.params.id);
+    let postContent;
+    if (props.match.params.id == 1) {
+        postContent = (
+            <div className="blog-archives-items">
+                Archives with id of {props.match.params.id}
+                <InitProject />
+                <NavLink className="back-home" to="/archives">
+                    <div className="back-home_title">Go Back</div>
+                </NavLink>
+            </div>
+        )
+    } else if (props.match.params.id == 2) {
+        postContent = (
+            <div className="blog-archives-items">
+                Archives with id of {props.match.params.id}
+
+                <NavLink className="back-home" to="/archives">
+                    <div className="back-home_title">Go Back</div>
+                </NavLink>
+            </div>
+        )
+    }
+
     return (
-        <div className="blog-archives-items">
-            Archives with id of {props.match.params.id}
-            <NavLink className="back-home" to="/archives">
-                <div className="back-home_title">Go Back</div>
-            </NavLink>
+        <div>
+           { postContent }
         </div>
-    );
+    )
+
 };
+
+// class ArchivesItem extends React.Component {
+//     render () {
+//         let postContent;
+//         console.log(this.props.match.params.id);
+//         if (this.props.match.params.id == 1) {
+//             postContent = (
+//                 <div className="blog-archives-items">
+//                 asdasdadas
+//                 </div>
+//             )
+//         } else if (this.props.match.params.id == 2) {
+//             postContent = (
+//                 <div className="blog-archives-items">
+//                     Archives with id of {this.props.match.params.id}
+
+//                     <NavLink className="back-home" to="/archives">
+//                         <div className="back-home_title">Go Back</div>
+//                     </NavLink>
+//                 </div>
+//             )
+//         }
+
+//         return (
+//             <div>
+//                 { postContent }
+//             </div>
+//         )
+//     }
+// }
 
 export default ArchivesItem;
