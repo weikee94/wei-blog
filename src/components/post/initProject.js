4321cbb30f03a9f44ad6db8182ca939395a67e7a
WeiKee <weikee070194@gmail.com>
2018-05-15 10:01:07 +0800

    update with new markdown plugin

--- src/components/post/initProject.js
+++ src/components/post/initProject.js
@@ -1,10 +1,13 @@
-import Markdown from 'markdown-to-jsx';
+// import Markdown from 'markdown-to-jsx';
 import React from 'react';
+const ReactMarkdown = require('react-markdown')
+
+const input = '# This is a header\n\nAnd this is a paragraph'
 
 const InitProject = () => {
     return (
         <div>
-            Post
+            <ReactMarkdown source={input} />
         </div>
     );
 };
