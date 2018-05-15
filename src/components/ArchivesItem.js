07aa0dfe4b9ec4d05fff7b5c15303f75c47b41cd
WeiKee <weikee070194@gmail.com>
2018-05-15 10:03:59 +0800

    move from post

--- src/components/ArchivesItem.js
+++ src/components/ArchivesItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import InitProject from '../components/Post/InitProject';
+import InitProject from '../components/InitProject';
 
 const ArchivesItem = (props) => {
     console.log(props.match.params.id);
