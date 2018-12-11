24af4b92e87cb4d36e327cb4ff21bf230c9e2902
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2018-12-11 10:10:35 +0800

    move the images folder

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = env => {
               loader: "file-loader",
               options: {
                 name: "[name].[ext]",
-                outputPath: "/images/"
+                outputPath: "images"
               }
             }
           ]
