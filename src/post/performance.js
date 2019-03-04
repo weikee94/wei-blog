0531bdc5b5a0e078d8ac66f00a603dd35bac5846
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-03-04 23:01:36 +0800

    update performance part one

---
+++ src/post/performance.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Performance extends Component {
+  render() {
+    return (
+      <div>
+        Performance
+        <div>
+          HTML SECTION
+          <ul>
+            <li>Load style tag in the head</li>
+            <li>Load script right before the body</li>
+          </ul>
+        </div>
+        <div>
+          CSS SECTION
+          <ul>
+            <li>Only load whatever is needed</li>
+            <li>Above the fold loading</li>
+            <li>Media attributes</li>
+            <li>Less specifity</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Performance;
