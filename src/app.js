a7ca8ee4ee08d0ec0b14a2a04cbb37639301fa4d
weikee <weikee070194@gmail.com>
2020-03-03 21:26:35 +0800

    restructure css base

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
-import WeiBlog from './components/WeiBlog';
-import './styles/base.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import AppRouter from "./routers/AppRouter";
+import WeiBlog from "./components/WeiBlog";
+import "./styles/styles.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
-
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
--- src/components/WeiBlog.js
+++ src/components/WeiBlog.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
 
 export default class WeiBlog extends React.Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <p>Wei's Blog</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="wei-wrapper">
+        <Header />
+        <p>Wei's Blog</p>
+      </div>
+    );
+  }
 }
