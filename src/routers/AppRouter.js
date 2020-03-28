587de7be53041f531b4f3f73249e7589203b1c1c
weikee <weikee070194@gmail.com>
2020-03-28 11:02:05 +0800

    fixed footer

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import Home from '../components/Home';
-import About from '../components/About';
-import Archives from '../components/Archives';
-import ArchivesItem from '../components/ArchivesItem';
-import Contact from '../components/Contact';
-import NotFound from '../components/NotFound';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import Home from "../components/Home";
+import About from "../components/About";
+import Archives from "../components/Archives";
+import ArchivesItem from "../components/ArchivesItem";
+import Contact from "../components/Contact";
+import NotFound from "../components/NotFound";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 // stateless function component
 const AppRouter = () => (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" component={Home} exact={true}/>
-                <Route path="/about" component={About} />
-                <Route path="/archives" component={Archives} exact={true}/>
-                <Route path="/archives/:id" component={ArchivesItem} />
-                <Route path="/contact" component={Contact} />
-                <Route component={NotFound} />
-            </Switch>
-            <Footer />
-        </div>
-    </BrowserRouter>
+  <BrowserRouter>
+    <div className="page-container">
+      <Header />
+      <div className="page-content">
+        <Switch>
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/about" component={About} />
+          <Route path="/archives" component={Archives} exact={true} />
+          <Route path="/archives/:id" component={ArchivesItem} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+      <Footer />
+    </div>
+  </BrowserRouter>
 );
 
 export default AppRouter;
