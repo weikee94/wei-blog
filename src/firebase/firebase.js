0d2d0ed51dd2087414d0ad4d5a7731fafdb015f8
weikee <weikee070194@gmail.com>
2020-03-31 16:57:06 +0800

    add firebase analytics

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,16 @@
 import * as firebase from "firebase/app";
 import "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDUkYIdGNxBm-kckDWP1YZSKBOmO9OENBo",
-  authDomain: "my-blog-2018.firebaseapp.com",
-  databaseURL: "https://my-blog-2018.firebaseio.com",
-  projectId: "my-blog-2018",
-  storageBucket: "my-blog-2018.appspot.com",
-  messagingSenderId: "974357428439",
-  appId: "1:974357428439:web:32a48e19fed1174de65892"
+var firebaseConfig = {
+  apiKey: "AIzaSyA-3g4qQJwoqtDqby_R6PKRIbgkNRxNrPs",
+  authDomain: "wei-blog-11952.firebaseapp.com",
+  databaseURL: "https://wei-blog-11952.firebaseio.com",
+  projectId: "wei-blog-11952",
+  storageBucket: "wei-blog-11952.appspot.com",
+  messagingSenderId: "782074666092",
+  appId: "1:782074666092:web:18c616d50aaba86e5f8c49",
+  measurementId: "G-37K95CW8MW"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
