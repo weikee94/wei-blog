7b6a82ca9ed30a78b89e1a82c56e0a1a011f0c80
weikee94 <weikee070194@gmail.com>
2020-10-08 20:36:03 +0800

    migrate to nextjs

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppRouter from "./routers/AppRouter";
-import WeiBlog from "./components/WeiBlog";
-import "./styles/styles.scss";
-import "./firebase/firebase";
-
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
--- src/context/notes-context.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-const NotesContext = React.createContext();
-
-export { NotesContext as default };
